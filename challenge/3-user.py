7aa61cbdfc6387235c6d10c5b4d152799ffa44d5
aasiria24 <amaalmoasiri@gmail.com>
2026-06-22 13:25:28 +0300

    Update 3-user.py

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -34,7 +34,7 @@ class User():
         if pwd is None or type(pwd) is not str:
             self.__password = None
         else:
-            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()  # ← __password, not _password
+            self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
     def is_valid_password(self, pwd):
         """
         Valid password:
@@ -47,7 +47,7 @@ class User():
             return False
         if self.__password is None:
             return False
-        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password  # ← .lower() not .upper()
+        return hashlib.md5(pwd.encode()).hexdigest().lower() == self.__password
 if __name__ == '__main__':
     print("Test User")
     user_1 = User()
